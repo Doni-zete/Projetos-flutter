d2d3d50f9fde70cfb0666307cf69253389c1639c
Doni-zete <donizete112@gmail.com>
2021-08-27 21:49:10 -0300

    concluido

---
+++ app_carros/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:app_carros/tela_principal.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Carros',
+    home: TelaPrincipal(),
+  ));
+}
+

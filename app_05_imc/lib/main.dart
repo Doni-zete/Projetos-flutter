b93bfb61e6023627d017a77cae230f8d4cbeffe5
Doni-zete <donizete112@gmail.com>
2021-09-02 21:30:28 -0300

    efetuar calculo

--- app_05_imc/lib/main.dart
+++ app_05_imc/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+ 
 import 'package:flutter/material.dart';
  
 void main(){
@@ -10,34 +12,36 @@ void main(){
       // TEMA
       //
       theme: ThemeData(
-        primaryColor: Colors.purple.shade900,
+        primaryColor: Colors.green.shade900,
         backgroundColor: Colors.grey.shade300,
+        focusColor: Colors.grey.shade600,
  
         textTheme: TextTheme(
           headline1: TextStyle(
             fontSize: 32,
-            color: Colors.purple.shade900
+            color: Colors.grey.shade900
           ),
  
           headline2: TextStyle(
             fontSize: 24,
-            color: Colors.purple.shade900,
+            color: Colors.green.shade900,
           ),
  
           headline3: TextStyle(
             fontSize: 20,
-            color: Colors.purple.shade600,
+            color: Colors.grey.shade500,
           ),
         ),
-
-        //aparencia do botão
+ 
+        //aparência do botão
         elevatedButtonTheme: ElevatedButtonThemeData(
-          style:ElevatedButton.styleFrom(
-            primary: Colors.purple.shade900,
-            textStyle:  TextStyle(
-              fontSize:24,
-            )
-          ))
+          style: ElevatedButton.styleFrom(
+            primary: Colors.green.shade900,
+            textStyle: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+        ),
  
       ),
  
@@ -57,6 +61,12 @@ class TelaPrincipal extends StatefulWidget {
 }
  
 class _TelaPrincipalState extends State<TelaPrincipal> {
+ 
+  //Declaração dos atributos (variáveis) que serão utilizados
+  //para controlar os valores informados nos campos de texto
+  var txtPeso = TextEditingController();
+  var txtAltura = TextEditingController();
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -87,9 +97,9 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
               color: Theme.of(context).primaryColor,
             ),
  
-            campoTexto('Peso'),
-            campoTexto('Altura'),
-            botao('Calcular'),
+            campoTexto('Peso', txtPeso),
+            campoTexto('Altura', txtAltura),
+            botao('calcular'),
  
           ],
         ),
@@ -101,45 +111,87 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
   //
   // CAMPO DE TEXTO para entrada de dados
   //
-  Widget campoTexto(rotulo){
+  Widget campoTexto(rotulo, variavel){
     return Container(
       padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
  
       child: TextFormField(
+        
+        keyboardType: TextInputType.number,
+ 
+        //Indicar a variável de controle
+        controller: variavel,
  
         style: Theme.of(context).textTheme.headline1,
-        
         decoration: InputDecoration(
           labelText: rotulo,
           labelStyle: Theme.of(context).textTheme.headline2,
-
-          hintText: "Entre com o valor",
+          hintText: 'Entre com o valor',
           hintStyle: Theme.of(context).textTheme.headline3,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12),
           ),
  
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Theme.of(context).focusColor,
+            ),
+            borderRadius: BorderRadius.circular(12),  
+          ),
+ 
         ),
       ),
- 
     );
   }
-
-
+ 
   //
-  //Botão
+  // BOTÃO
   //
   Widget botao(rotulo){
     return Container(
       width: 250,
-      height: 80,
-      padding: EdgeInsets.only(top:20),
-      child:  ElevatedButton(
+      height: 70,
+      padding: EdgeInsets.only(top: 20),
+      child: ElevatedButton(
         child: Text(rotulo),
         onPressed: (){
-          print('botao calcular pressionado!');
+          //print('botão calcular pressionado!');
+ 
+          //O método setState permite a manipulação do estado
+          //da UI do App
+          setState(() {
+            double peso = double.parse(txtPeso.text.replaceFirst(',', '.'));
+            double altura = double.parse(txtAltura.text.replaceFirst(',', '.'));
+            double imc = peso / pow(altura, 2);
+            
+            caixaDialogo('IMC: ${imc.toStringAsFixed(2)}');
+          });
+ 
         },
       ),
+    );
+  }
+ 
+  //
+  // CAIXA DE DIÁLOGO
+  //
+  caixaDialogo(msg){
+    return showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text('Resultado'),
+          content: Text(msg),
+          actions: [
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              }, 
+              child: Text('fechar'),
+            ),
+          ],
+        );
+      }
     );
   }
  

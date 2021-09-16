eab672472a55c02ad65edd70363ab06e66a30259
Doni-zete <donizete112@gmail.com>
2021-09-16 20:15:00 -0300

    pagiview

--- app_bottom_navigation_bar/lib/main.dart
+++ app_bottom_navigation_bar/lib/main.dart
@@ -21,16 +21,59 @@ class TelaPrincipal extends StatefulWidget {
 }
 
 class _TelaPrincipalState extends State<TelaPrincipal> {
+  
+  //Index da tela que será carregada
+  var telaAtual = 0;
+  
+  //Controlador utilizado na troca das telas (páginas)
+  var pageController = PageController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
       //
+      // BODY
+      //
+      body: PageView(
+        controller: pageController,
+        children: [
+          TelaHome(),
+          TelaPesquisar(),
+          TelaNotificacoes(),
+          TelaConfiguracoes(),
+        ],
+
+    //Mudança de pagina: quando ocorrer a mudança de pagina
+    //por meio da interação do usuario, e necessario
+    //atualizar o valor da variavel telaAtual
+    onPageChanged: (index){
+      setState(() {
+        telaAtual =index;
+      });
+    },
+
+
+      ),
+
+      //
       // BARRA DE NAVEGAÇÃO
       //
       bottomNavigationBar: BottomNavigationBar(
+
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.blue.shade900,
+
+        iconSize: 48,
+
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(0.40),
+
+        selectedFontSize: 16,
+        unselectedFontSize: 16,
+
+        //Index do botão selecionado
+        currentIndex: telaAtual,
 
         items:[
           BottomNavigationBarItem(
